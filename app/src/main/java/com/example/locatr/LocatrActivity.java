6e90d7067abb20d7d181db9051d889adf17c221e
mengzicheng <724686158@qq.com>
2016-10-21 11:01:02 +0800

    改了过时API

--- app/src/main/java/com/example/locatr/LocatrActivity.java
+++ app/src/main/java/com/example/locatr/LocatrActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 
 public class LocatrActivity extends SingleFragmentActivity {
@@ -19,9 +20,9 @@ public class LocatrActivity extends SingleFragmentActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        int errorCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        int errorCode = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
         if (errorCode != ConnectionResult.SUCCESS) {
-            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(errorCode, this, REQUEST_ERROR,
+            Dialog errorDialog = GoogleApiAvailability.getInstance().getErrorDialog(this, errorCode, REQUEST_ERROR,
                     new DialogInterface.OnCancelListener() {
 
                         @Override
